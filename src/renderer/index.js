93b334b7c3b7414db4ade8d2593dc8110f46d11c
mkwtys <mkwtys@gmail.com>
2017-04-21 13:02:25 +0900

    chore: build to ./app

---
+++ src/renderer/index.js
@@ -0,0 +1,9 @@
+/* @flow */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorldComponent from './ui/HelloWorldComponent';
+
+ReactDOM.render(
+  <HelloWorldComponent {...process.versions} />,
+  document.querySelector('.root')
+);
---
+++ src/renderer/ui/HelloWorldComponent.js
@@ -0,0 +1,13 @@
+/* @flow */
+import React from 'react';
+
+export default function HelloWorldComponent(props) {
+  return (
+    <div>
+      <div>Hello World!</div>
+      <div>We are using Node.js {props.node},</div>
+      <div>Chromium {props.chrome},</div>
+      <div>and Electron {props.electron}.</div>
+    </div>
+  );
+}
