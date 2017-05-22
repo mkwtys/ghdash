0ca5345b803eae657a92159ef0438150bfde5a70
mkwtys <mkwtys@gmail.com>
2017-05-22 13:37:36 +0900

    test: ConfigRepository (#10)

---
+++ test/util/isElectron.js
@@ -0,0 +1,27 @@
+import assert from 'assert';
+import isElectron from 'util/isElectron';
+
+describe('util/isElectron', () => {
+  it('should return false in Node.js', () => {
+    assert(isElectron() === false);
+  });
+
+  it('should return true when window.process.type is renderer', () => {
+    let orgWindowProcess;
+    let orgWindowProcessType;
+    if (window.process && window.process.type) {
+      orgWindowProcess = window.process;
+      orgWindowProcessType = window.process.type;
+    }
+    window.process = {
+      type: 'renderer'
+    };
+    assert(isElectron() === true);
+    if (orgWindowProcess) {
+      window.process = orgWindowProcess;
+    }
+    if (orgWindowProcessType) {
+      window.process.type = orgWindowProcessType;
+    }
+  });
+});
